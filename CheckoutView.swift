4673eba869c4b68edad6b4e24e048d3a17cde510
Carlos Eduardo Witte <cewitte@icloud.com>
2025-02-25 19:47:14 -0300

    added Paul Hudson's complete project code

---
+++ CheckoutView.swift
@@ -0,0 +1,75 @@
+//
+//  CheckoutView.swift
+//  CupcakeCorner
+//
+//  Created by Carlos Eduardo Witte on 24/02/25.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    var order: Order
+    
+    @State private var confirmationMessage = ""
+    @State private var showConfirmation = false
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                AsyncImage(url: URL(string: "https://hws.dev/img/cupcakes@3x.jpg"), scale: 3) { image in
+                    image
+                        .resizable()
+                        .scaledToFit()
+                } placeholder: {
+                    ProgressView()
+                }
+                .frame(height: 233)
+                
+                Text("Your total cost is \(order.cost, format: .currency(code: "USD"))")
+                    .font(.title)
+                
+                Button("Place Order") {
+                    Task {
+                        await placeOrder()
+                    }
+                }
+                .padding()
+            }
+        }
+        .navigationTitle("Checkout")
+        .navigationBarTitleDisplayMode(.inline)
+        .scrollBounceBehavior(.basedOnSize)
+        .alert("Thank you!", isPresented: $showConfirmation) {
+            Button("OK") {}
+        } message: {
+            Text(confirmationMessage)
+        }
+    }
+    
+    func placeOrder() async {
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+        
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue( "application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        
+        do {
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
+            
+            // handle our result
+            let decodedOrder = try JSONDecoder().decode(Order.self, from: data)
+            confirmationMessage = "Your order for \(decodedOrder.quantity) x \(Order.types[decodedOrder.type].lowercased()) is on its way!"
+            showConfirmation = true
+        } catch {
+            print("Check out failed: \(error.localizedDescription)")
+        }
+    }
+}
+
+#Preview {
+    CheckoutView(order: Order())
+}
--- README.md
+++ README.md
@@ -2,12 +2,14 @@
 
 ## Project 10
 
-A multi-screen app for ordering cupcakes. This will use a couple of forms, but it also shows how to send and receive the order data from the internet, how to validate forms, and more.
+"A multi-screen app for ordering cupcakes. This will use a couple of forms, but it also shows how to send and receive the order data from the internet, how to validate forms, and more.
 
-As we continue to dig deeper and deeper into `Codable`, we'll see how flexible and safe it is. In particular, it is very different it is from the much older `UserDefaults` API – it’s so nice not having to worry about typing strings exactly correctly!
+As we continue to dig deeper and deeper into `Codable`, we'll see how flexible and safe it is. In particular, it is very different it is from the much older `UserDefaults` API – it’s so nice not having to worry about typing strings exactly correctly!"
+
+The `main` branch contains Paul Hudson's complete code minus the challenges, which are in their own branches.
 
 Source URL: [Cupcake Corner: Introduction](https://www.hackingwithswift.com/books/ios-swiftui/cupcake-corner-introduction)
 
-Thank you [Paul Hudson - @twostraws](https://x.com/twostraws)
+Original code created by: [Paul Hudson - @twostraws](https://x.com/twostraws) (Thank you!)
 
 Made with :heart: by [@cewitte](https://x.com/cewitte)
